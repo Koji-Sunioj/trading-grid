e0f9273b7a58fa6a62df5fbd7a5889f4fb3eb5b6
Koji-Sunioj <koji.gabriel218@gmail.com>
2025-03-30 19:35:41 +0300

    route key

--- lambdas/auth.py
+++ lambdas/auth.py
@@ -4,24 +4,20 @@ import json
 
 dynamodb = boto3.resource('dynamodb')
 
-# hello
+response_object = {}
+response_object['headers'] = {"Content-Type": "application/json",
+                              "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "*"}
 
 
 def handler(event, context):
-    print(os.environ.get('TABLE_NAME'))
-    print(event['resource'])
-    print("/")
-    print(context)
-    table = dynamodb.Table(os.environ.get('TABLE_NAME'))
-    results = table.scan()
-    print(results)
+    route_key = "%s %s" % (event["httpMethod"], event['resource'])
+    # table = dynamodb.Table(os.environ.get('TABLE_NAME'))
+    # results = table.scan()
+    # print(results)
     # Return response object
-    response_object = {}
+    print(route_key)
+
     response_object['statusCode'] = 200
-    response_object['headers'] = {}
-    response_object['headers']['Content-Type'] = 'application/json'
-    response_object['headers']["Access-Control-Allow-Origin"] = "*"
-    response_object['headers']["Access-Control-Allow-Methods"] = "*"
     response_object['body'] = json.dumps(
         {"resource": event['resource'], "method": event["httpMethod"]})
 
