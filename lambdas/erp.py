ea94c80a39a46081138b40ca8f2b422d05bb60f8
Koji-Sunioj <koji.gabriel218@gmail.com>
2025-04-21 20:07:10 +0300

    signing

--- lambdas/erp.py
+++ lambdas/erp.py
@@ -1,18 +1,15 @@
+from jose import jwt
+import requests
 import os
 import boto3
 import json
-import requests
-from jose import jwt
+
 
 response = {}
 response['headers'] = {"Access-Control-Allow-Methods": "*"}
 
 
-def handler(event):
-    print(os.environ.get("SIGNING_URL"))
-    print(os.environ.get("USER_POOL_ID"))
-    print(event)
-
+def handler(event, context):
     if "Authorization" not in event["headers"]:
         response["statusCode"] = 401
         response["body"] = json.dumps({"message": "not authorized"})
@@ -25,15 +22,13 @@ def handler(event):
     rsa_key = [header for header in kids if header["kid"]
                == token_header["kid"]][0]
 
-    payload = jwt.decode(
+    jwt.decode(
         token,
         rsa_key,
         algorithms=["RS256"],
         issuer=signing_url,
-        # subject=os.environ.get("USER_POOL_ID")
+        subject=os.environ.get("M2M_POOL_ID")
     )
-
-    print(payload)
 
     response["statusCode"] = 200
     response["body"] = json.dumps({"message": "hey"})
