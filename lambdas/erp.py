d9da2d7bc13f122d8c568dd9dcd018a4ca71fd02
Koji-Sunioj <koji.gabriel218@gmail.com>
2025-04-23 22:44:17 +0300

    ddb

--- lambdas/erp.py
+++ lambdas/erp.py
@@ -1,12 +1,29 @@
-from jose import jwt
-import requests
 import os
-import boto3
 import json
+import boto3
+import requests
+from jose import jwt
+from decimal import Decimal
 
 
 response = {}
 response['headers'] = {"Access-Control-Allow-Methods": "*"}
+
+
+def check_M2M_token(authoriziation_value):
+    token = authoriziation_value
+    signing_url = os.environ.get("SIGNING_URL")
+    kids = requests.get(signing_url + "/.well-known/jwks.json").json()["keys"]
+    token_header = jwt.get_unverified_header(token)
+    rsa_key = [header for header in kids if header["kid"]
+               == token_header["kid"]][0]
+    jwt.decode(
+        token,
+        rsa_key,
+        algorithms=["RS256"],
+        issuer=signing_url,
+        subject=os.environ.get("M2M_POOL_ID")
+    )
 
 
 def handler(event, context):
@@ -14,33 +31,40 @@ def handler(event, context):
         if "Authorization" not in event["headers"]:
             raise Exception("no authoriziation")
 
-        token = event["headers"]["Authorization"]
-        signing_url = os.environ.get("SIGNING_URL")
-        kids = requests.get(
-            signing_url + "/.well-known/jwks.json").json()["keys"]
-        token_header = jwt.get_unverified_header(token)
-        rsa_key = [header for header in kids if header["kid"]
-                   == token_header["kid"]][0]
+        check_M2M_token(event["headers"]["Authorization"])
 
-        jwt.decode(
-            token,
-            rsa_key,
-            algorithms=["RS256"],
-            issuer=signing_url,
-            subject=os.environ.get("M2M_POOL_ID")
+        body = None
+
+        if event["body"] != None:
+            body = json.loads(event["body"], parse_float=Decimal)
+            body["amendments"] = []
+
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(os.environ.get("TABLE_NAME"))
+
+        ddb_response = table.get_item(
+            Key={'purchase_order_id': int(body["purchase_order_id"])})
+
+        if "Item" in ddb_response:
+            raise Exception("that purchase order already exists")
+
+        ddb_response = table.put_item(
+            TableName=os.environ.get("TABLE_NAME"),
+            Item=body,
         )
 
-        body = json.loads(event["body"])
-        print(body)
+        if ddb_response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+            raise Exception("there was an error creating that purchase order")
 
         response["statusCode"] = 200
-        response["body"] = json.dumps({"message": "hey"})
+        response["body"] = json.dumps(
+            {"message": "purchase order %s received" % body["purchase_order_id"]})
 
     except Exception as error:
         print("error name %s" + error.__class__.__name__)
         print(error)
         print(error.__str__())
-        error_message = "an error occurred."
+        error_message = error.__str__()
 
         response['statusCode'] = 400
         response["body"] = json.dumps({"message": error_message})
