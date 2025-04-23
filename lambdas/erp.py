8b84b929e77e5212cf7dfcc096e5bc8aaa308969
Koji-Sunioj <koji.gabriel218@gmail.com>
2025-04-23 20:53:55 +0300

    yodate

--- lambdas/erp.py
+++ lambdas/erp.py
@@ -10,26 +10,39 @@ response['headers'] = {"Access-Control-Allow-Methods": "*"}
 
 
 def handler(event, context):
-    if "Authorization" not in event["headers"]:
-        response["statusCode"] = 401
-        response["body"] = json.dumps({"message": "not authorized"})
-        return response
+    try:
+        if "Authorization" not in event["headers"]:
+            raise Exception("no authoriziation")
 
-    token = event["headers"]["Authorization"]
-    signing_url = os.environ.get("SIGNING_URL")
-    kids = requests.get(signing_url + "/.well-known/jwks.json").json()["keys"]
-    token_header = jwt.get_unverified_header(token)
-    rsa_key = [header for header in kids if header["kid"]
-               == token_header["kid"]][0]
+        token = event["headers"]["Authorization"]
+        signing_url = os.environ.get("SIGNING_URL")
+        kids = requests.get(
+            signing_url + "/.well-known/jwks.json").json()["keys"]
+        token_header = jwt.get_unverified_header(token)
+        rsa_key = [header for header in kids if header["kid"]
+                   == token_header["kid"]][0]
 
-    jwt.decode(
-        token,
-        rsa_key,
-        algorithms=["RS256"],
-        issuer=signing_url,
-        subject=os.environ.get("M2M_POOL_ID")
-    )
+        jwt.decode(
+            token,
+            rsa_key,
+            algorithms=["RS256"],
+            issuer=signing_url,
+            subject=os.environ.get("M2M_POOL_ID")
+        )
 
-    response["statusCode"] = 200
-    response["body"] = json.dumps({"message": "hey"})
+        body = json.loads(event["body"])
+        print(body)
+
+        response["statusCode"] = 200
+        response["body"] = json.dumps({"message": "hey"})
+
+    except Exception as error:
+        print("error name %s" + error.__class__.__name__)
+        print(error)
+        print(error.__str__())
+        error_message = "an error occurred."
+
+        response['statusCode'] = 400
+        response["body"] = json.dumps({"message": error_message})
+
     return response
