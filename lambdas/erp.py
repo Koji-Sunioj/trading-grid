60e940fc24f6d7158de98fb3a1a0273d031cb962
Koji-Sunioj <koji.gabriel218@gmail.com>
2025-04-28 22:17:27 +0300

    hey

--- lambdas/erp.py
+++ lambdas/erp.py
@@ -31,38 +31,46 @@ def handler(event, context):
         if "Authorization" not in event["headers"]:
             raise Exception("no authoriziation")
 
-        check_M2M_token(event["headers"]["Authorization"])
+        route_key = "%s %s" % (event["httpMethod"], event['resource'])
 
-        body = None
+        match route_key:
+            case "POST /purchase-orders":
 
-        if event["body"] != None:
-            body = json.loads(event["body"], parse_float=Decimal)
-            body["amendments"] = []
+                check_M2M_token(event["headers"]["Authorization"])
 
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table(os.environ.get("TABLE_NAME"))
+                body = None
+                existing_ammendments = None
 
-        ddb_response = table.get_item(
-            Key={'purchase_order_id': int(body["purchase_order_id"])})
+                if event["body"] != None:
+                    body = json.loads(event["body"], parse_float=Decimal)
+                    body["amendments"] = []
 
-        if "Item" in ddb_response:
-            raise Exception("that purchase order already exists")
+                dynamodb = boto3.resource('dynamodb')
+                table = dynamodb.Table(os.environ.get("TABLE_NAME"))
 
-        ddb_response = table.put_item(
-            TableName=os.environ.get("TABLE_NAME"),
-            Item=body,
-        )
+                ddb_response = table.get_item(
+                    Key={'purchase_order_id': int(body["purchase_order_id"])})
 
-        if ddb_response["ResponseMetadata"]["HTTPStatusCode"] != 200:
-            raise Exception("there was an error creating that purchase order")
+                if "Item" in ddb_response:
+                    body["amendments"] = ddb_response["Item"]["amendments"]
 
-        response["statusCode"] = 200
-        response["body"] = json.dumps(
-            {"message": "purchase order %s received" % body["purchase_order_id"]})
+                ddb_response = table.put_item(
+                    TableName=os.environ.get("TABLE_NAME"),
+                    Item=body,
+                )
+
+                if ddb_response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+                    raise Exception(
+                        "there was an error creating that purchase order")
+
+                response["statusCode"] = 200
+                response["body"] = json.dumps(
+                    {"message": "purchase order %s received" % body["purchase_order_id"]})
+            case _:
+                raise Exception("no matching resource")
 
     except Exception as error:
         print("error name %s" + error.__class__.__name__)
-        print(error)
         print(error.__str__())
         error_message = error.__str__()
 
