96a5f97d54e994a2fdd15daf82fb326babbe79b6
Koji-Sunioj <koji.gabriel218@gmail.com>
2025-05-06 23:29:58 +0300

    hey

--- lambdas/auth.py
+++ lambdas/auth.py
@@ -1,6 +1,9 @@
 import os
-import boto3
 import json
+import boto3
+import base64
+from jose import jwt
+from datetime import timedelta, datetime, timezone
 
 response = {}
 response['headers'] = {"Access-Control-Allow-Methods": "*"}
@@ -8,12 +11,33 @@ response['headers'] = {"Access-Control-Allow-Methods": "*"}
 
 def handler(event, context):
     try:
-        response["headers"]["Access-Control-Allow-Origin"] = event["headers"]["origin"]
+        if "origin" in event["headers"]:
+            response["headers"]["Access-Control-Allow-Origin"] = event["headers"]["origin"]
         response["headers"]["Access-Control-Allow-Credentials"] = "true"
         cognito = boto3.client("cognito-idp")
         route_key = "%s %s" % (event["httpMethod"], event['resource'])
 
         match route_key:
+            case "POST /auth/client":
+
+                allowed_creds = json.loads(base64.b64decode(
+                    os.environ.get("CLIENT_CREDS")).decode("utf-8"))
+                creds = base64.b64decode(event["headers"]["Authorization"].split(" ")[
+                                         1]).decode("utf-8").split(":")
+                client_id, client_secret = creds[0], creds[1]
+
+                if allowed_creds[client_id] == client_secret:
+                    now = datetime.now(timezone.utc)
+                    expires = now + timedelta(minutes=10)
+                    jwt_payload = {"sub": "m2m-api", "client_id": client_id, "aud": event["resource"],
+                                   "iat": now, "exp": expires}
+                    token = jwt.encode(
+                        jwt_payload, os.environ.get("TOKEN_KEY"))
+                    response["statusCode"] = 200
+                    response["body"] = json.dumps({"token": token})
+                else:
+                    raise Exception("invalid credentials")
+
             case "GET /auth/merchant":
 
                 if "cookie" not in event["headers"]:
@@ -22,6 +46,8 @@ def handler(event, context):
                 token = event["headers"]["cookie"].split("=")[1]
                 cognito_response = cognito.get_user(AccessToken=token)
                 response["statusCode"] = 200
+                response["body"] = json.dumps(
+                    {"user": cognito_response["Username"]})
 
             case "POST /auth/sign-in":
                 if event["body"] != None:
