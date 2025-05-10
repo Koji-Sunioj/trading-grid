9730629927b8ab8393d200042040ffa0a01b3857
Koji-Sunioj <koji.gabriel218@gmail.com>
2025-05-10 09:43:39 +0300

    grid

--- frontend/src/pages/erp-module.jsx
+++ frontend/src/pages/erp-module.jsx
@@ -4,11 +4,11 @@ import { useEffect, useState } from "react";
 export const ERP = () => {
   const { module } = useParams();
 
-  const [orders, setOrders] = useState(null);
+  const [purchaseOrders, setPurchaseOrders] = useState(null);
 
   useEffect(() => {
     module === "orders" &&
-      orders === null &&
+      purchaseOrders === null &&
       (async () => {
         const response = await fetch(
           import.meta.env.VITE_API + `/purchase-orders/merchant`,
@@ -18,15 +18,14 @@ export const ERP = () => {
           }
         );
         const { status } = response;
-        setOrders([]);
-        console.log(status);
 
-        /*  if (status !== 200) {
-          setAuthorized({ message: "unathorized", state: false, user: null });
+        if (status !== 200) {
+          setPurchaseOrders([]);
         } else {
-          const { user } = await response.json();
-          setAuthorized({ message: "authorized", state: true, user: user });
-        } */
+          const { orders } = await response.json();
+          console.log(orders);
+          setPurchaseOrders(orders);
+        }
       })();
   });
 
@@ -35,6 +34,31 @@ export const ERP = () => {
       <div class="has-text-centered mb-4">
         <h1 class="title">module: {module}</h1>
         <h2 class="subtitle">Click on a module below to get started.</h2>
+      </div>
+      <div className="po-table">
+        {purchaseOrders !== null && purchaseOrders.length > 0 && (
+          <table class="table">
+            <thead>
+              <tr>
+                <th>last modified</th>
+                <th>order id</th>
+                <th>line count</th>
+              </tr>
+            </thead>
+            <tbody>
+              {purchaseOrders.map((order) => {
+                const { modified, data, purchase_order_id } = order;
+                return (
+                  <tr key={purchase_order_id}>
+                    <td>{modified}</td>
+                    <td>{purchase_order_id}</td>
+                    <td>{data.length}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
