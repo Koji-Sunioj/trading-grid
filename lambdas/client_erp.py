e4a010a3ee857f90cc0a08707f1f87ecb229a855
Koji-Sunioj <koji.gabriel218@gmail.com>
2026-01-19 18:22:01 +0200

    finally

--- lambdas/client_erp.py
+++ lambdas/client_erp.py
@@ -1,17 +1,12 @@
 import os
-import hmac
-import math
 import json
 import boto3
-import hashlib
-import datetime
-import requests
 import traceback
 
+from utils import check_hmac, get_dispatch, search
+
 from functools import wraps
-from zoneinfo import ZoneInfo
 from decimal import Decimal
-from datetime import datetime, timedelta, date
 from boto3.dynamodb.conditions import Attr
 
 dynamodb = boto3.resource('dynamodb')
@@ -20,52 +15,6 @@ routing_table = dynamodb.Table(os.environ.get("ROUTING_TABLE"))
 dispatch_table = dynamodb.Table(os.environ.get("DISPATCH_TABLE"))
 ammendments_table = dynamodb.Table(
     os.environ.get("PO_AMMENDMENT_TABLE"))
-
-
-def serialize_float(obj):
-    return float(obj)
-
-
-def check_hmac(payload, request_hmac, hmac_key):
-    correct_hmac = hmac.digest(hmac_key.encode(
-    ), payload.encode(), digest=hashlib.sha256).hex()
-    if not hmac.compare_digest(request_hmac, correct_hmac):
-        raise Exception("invalid credentials")
-
-
-def search(dicts, key, value):
-    try:
-        return next(n for n in dicts if n[key] == value)
-    except:
-        return None
-
-
-def get_dispatch(items, client):
-    lat, long = client["coords"]["latitude"], client["coords"]["longitude"]
-
-    distance_lookup = requests.get("https://api.radar.io/v1/route/distance?origin=%s&destination=%s,%s&modes=car&units=metric" % (
-        os.environ.get("STORE_COORDS"), lat, long), headers={"Authorization": os.environ.get("TOKEN_KEY")})
-
-    freight = distance_lookup.json()["routes"]["car"]
-    kilometers, minutes = int(
-        freight["distance"]["value"] / 1000), int(round(freight["duration"]["value"]))
-    weight_grams = 100 * \
-        int(items)
-    volume = (14.2 * 12.5 * 1.0) / 5000 * \
-        float(items)
-    cost = round(kilometers * volume * 1.25, 2)
-
-    today = date.today()
-    current_slot = datetime(year=today.year, month=today.month, day=today.day,
-                            hour=12, minute=0, tzinfo=ZoneInfo("Europe/Helsinki"))
-    n_days = 7 if current_slot.weekday() >= 2 else 2
-
-    dispatch_slot = current_slot + \
-        timedelta(days=n_days - current_slot.weekday())
-    estimated_delivery = dispatch_slot + \
-        timedelta(minutes=math.ceil(minutes / 10) * 10)
-
-    return {"freight_cost": cost, "estimated_delivery": estimated_delivery.strftime("%Y-%m-%d %H:%M"), "weight_grams": weight_grams}
 
 
 def validate(function):
