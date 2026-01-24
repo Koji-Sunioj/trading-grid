a1371ecd115075290f5718d802a049cfd46c1208
Koji-Sunioj <koji.gabriel218@gmail.com>
2026-01-24 16:03:46 +0200

    hey

--- lambdas/utils.py
+++ lambdas/utils.py
@@ -9,6 +9,15 @@ from zoneinfo import ZoneInfo
 from datetime import datetime, timedelta, date
 
 
+def handler():
+    return (os.environ.get("STORE_COORDS"), os.environ.get("TOKEN_KEY"))
+
+
+store_coords, token_key = handler()
+print(store_coords)
+print(token_key)
+
+
 def serialize_float(obj):
     return float(obj)
 
@@ -32,6 +41,10 @@ def get_dispatch(items, client):
 
     distance_lookup = requests.get("https://api.radar.io/v1/route/distance?origin=%s&destination=%s,%s&modes=car&units=metric" % (
         os.environ.get("STORE_COORDS"), lat, long), headers={"Authorization": os.environ.get("TOKEN_KEY")})
+
+    print(distance_lookup.url)
+    freight = distance_lookup.json()
+    print(freight)
 
     freight = distance_lookup.json()["routes"]["car"]
     kilometers, minutes = int(
