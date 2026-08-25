7717eb5ad8b057e2c826c53c57cea1fa3db690d9
Koji-Sunioj <koji.gabriel218@gmail.com>
2026-08-25 19:56:42 +0300

    socket update

--- lambdas/socket.py
+++ lambdas/socket.py
@@ -1,7 +1,12 @@
+import os
 import json
 import boto3
 import datetime
+import traceback
 
+dynamodb = boto3.resource('dynamodb')
+sockets_table = dynamodb.Table(os.environ.get("SOCKETS_TABLE"))
+routing_table = dynamodb.Table(os.environ.get("ROUTING_TABLE"))
 
 def handler(event, context):
     # dynamodb = boto3.resource('dynamodb')
@@ -10,37 +15,52 @@ def handler(event, context):
     route_key = event["requestContext"]["routeKey"]
     connection_id = event["requestContext"]["connectionId"]
 
-    match route_key:
-        case "$connect":
-            if "queryStringParameters" in event and "user" in event["queryStringParameters"]:
+    try:
+        match route_key:
+            case "$connect":
+                #for client - use routing table and validate with hmac in utils
+                #for merchant - use cognito
+                #distinguist by url param
+                if "queryStringParameters" in event and "user" in event["queryStringParameters"]:
+                    print("hello")
+                    # clients = routing_table.scan()["Items"]
+                    # table.put_item(Item={"connection_id": connection_id, "user": event["queryStringParameters"]["user"], "at": datetime.datetime.now(
+                    # ).strftime("%Y-%m-%d %H:%M:%S")})
+                else:
+                    raise Exception("i need a name")
+                return {"statusCode": 200}
+
+            case "update":
                 print("hello")
-                # table.put_item(Item={"connection_id": connection_id, "user": event["queryStringParameters"]["user"], "at": datetime.datetime.now(
-                # ).strftime("%Y-%m-%d %H:%M:%S")})
-            else:
-                raise Exception("i need a name")
+                #payload = json.loads(event["body"])
+                #if "message" in payload:
+                #    user = table.get_item(Key={"connection_id": connection_id})[
+                #        "Item"]["user"]
+                #
+                #    message = payload["message"]
+                #    api_client = boto3.client("apigatewaymanagementapi", endpoint_url="https://%s/%s" % (
+                #        event["requestContext"]["domainName"], event["requestContext"]["stage"]))
+                #    connections = table.scan()
+                #    connection_ids = [item["connection_id"]
+                #                      for item in connections["Items"]]
+                #
+                #    for connection in connection_ids:
+                #        api_client.post_to_connection(
+                #            Data=json.dumps({"message": message, "user": user}), ConnectionId=connection)
+                #else:
+                #    raise Exception("i need a message")
+                return {"statusCode": 200}
+            case "$disconnect":
+                print("hello")
+                #table.delete_item(Key={"connection_id": connection_id})
+                return {"statusCode": 200}
+            
+    except Exception as error:
+        print("error name %s" % error.__class__.__name__)
+        print(traceback.format_exc())
+        # error_message = error.__str__()
 
-        case "update":
-            print("hello")
-            #payload = json.loads(event["body"])
-            #if "message" in payload:
-            #    user = table.get_item(Key={"connection_id": connection_id})[
-            #        "Item"]["user"]
-            #
-            #    message = payload["message"]
-            #    api_client = boto3.client("apigatewaymanagementapi", endpoint_url="https://%s/%s" % (
-            #        event["requestContext"]["domainName"], event["requestContext"]["stage"]))
-            #    connections = table.scan()
-            #    connection_ids = [item["connection_id"]
-            #                      for item in connections["Items"]]
-            #
-            #    for connection in connection_ids:
-            #        api_client.post_to_connection(
-            #            Data=json.dumps({"message": message, "user": user}), ConnectionId=connection)
-            #else:
-            #    raise Exception("i need a message")
+        return {"statusCode": 400}
 
-        case "$disconnect":
-            print("hello")
-            #table.delete_item(Key={"connection_id": connection_id})
 
-    return {"statusCode": 200}
+    
